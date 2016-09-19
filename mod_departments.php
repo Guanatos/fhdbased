cab3598fcd500803050493996044d576825d4e7c
Daniel Nuno <daniel.a.nuno.ramirez@intel.com>
2016-09-19 16:27:27 -0500

    avoid duplicate devices

--- mod_departments.php
+++ mod_departments.php
@@ -6,63 +6,60 @@ include("includes/checksessionadmin.php");
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Edit Skills</title>
+	<title>Edit Departments</title>
 <?php
 include("fhd_config.php");
 include("includes/header.php");
 include("includes/all-nav.php");
+include("includes/functions.php");
 include("includes/ez_sql_core.php");
 include("includes/ez_sql_mysqli.php");
-include("includes/functions.php");
 $db = new ezSQL_mysqli(db_user,db_password,db_name,db_host);
 $actionstatus = "";
+$type = 1; // Departments
 // <UPDATE>
 if (isset($_POST['nacl'])){
  if ( $_POST['nacl'] == md5(AUTH_KEY.$db->get_var("SELECT last_login FROM site_users WHERE user_id = $user_id;")) ) {
 	//authentication verified, continue.
-        $skill_id = $db->escape($_POST['skill_id']);
-        $skill_name = $db->escape($_POST['skill_name']);
-        $skill_desc = $db->escape($_POST['skill_desc']);
+        $type_id = $db->escape($_POST['type_id']);
+        $type_name = $db->escape($_POST['type_name']);
 // See if the record is not duplicated
-	if (ifexist('skills','skill_name',$skill_name)){
+	if (ifexist('site_types','type_name',$type_name)){
 // Record Found
         $actionstatus = "<div class=\"alert alert-danger\" style=\"max-width: 250px;\">
         <button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>
         Record duplicated, nothing was changed.
-	</div>";
+	    </div>";
 // Record Not Found
 	} else { 
-	$db->query("UPDATE skills SET skill_name='$skill_name',skill_desc='$skill_desc' WHERE skill_id = $skill_id;");
+	    $db->query("UPDATE site_types SET type_name='$type_name' WHERE type_id = $type_id;");
         $actionstatus = "<div class=\"alert alert-success\" style=\"max-width: 250px;\">
         <button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>
-        Skill Updated.
+        Record Updated.
         </div>";
 	}
  } // if
 }
 // </UPDATE>
-$skill_id = checkid($_GET['skill_id']);
-$num = $db->get_var("SELECT count(skill_id) FROM skills WHERE skill_id LIKE '$skill_id';");
+$type_id = checkid($_GET['type_id']);
+$num = $db->get_var("SELECT count(type_id) FROM site_types WHERE type_id LIKE '$type_id';");
 //$db->debug();
 $nacl = md5(AUTH_KEY.$db->get_var("SELECT last_login FROM site_users WHERE user_id = $user_id;"));
 ?>
-<h4>Edit Type</h4>
+<h4>Edit Department</h4>
 <?php 
 echo $actionstatus;
 echo "<p><a href='fhd_settings.php'>Settings</a></p>";
 ?>
 <?php if ($num > 0) { 
-	$skills = $db->get_results("SELECT skill_id,skill_name,skill_desc FROM skills WHERE skill_id LIKE '$skill_id' ORDER BY skill_name;");
+	$skills = $db->get_results("SELECT type_id,type_name FROM site_types WHERE type_id LIKE '$type_id' ORDER BY type_name;");
 	echo "<form action='' method='post' class='form-horizontal'>\n";
 	foreach ( $skills as $skill ) {
-	$skill_id = $skill->skill_id;
-	$skill_name = $skill->skill_name;
-	$skill_desc = $skill->skill_desc;
+	$type_id = $skill->type_id;
+	$type_name = $skill->type_name;
 	echo "<table class='<?php echo $table_style_2;?>' style='width: auto;'>";
-	echo "<tr><td>Skill Name*</td>";
-	echo "<td><input type='text' name='skill_name' value='$skill_name' required></td></tr>\n";
-	echo "<tr><td>Description*</td>\n";
-	echo "<td><textarea rows='2' name='skill_desc' required>$skill_desc</textarea></td></tr>\n";
+	echo "<tr><td>Department Name*</td>";
+	echo "<td><input type='text' name='type_name' value='$type_name' required></td></tr>\n";
 	echo "<tr><td colspan='2'><input type='submit' value='update' class='btn btn-primary'></td></tr>\n";
 	echo "</table>\n</form>\n";
 	} //foreach
@@ -74,3 +71,4 @@ if(isset($_SESSION['name'])){
 	echo "<br /><p><strong>Login Name:</strong> " . $_SESSION['name'] . "</p>";
 }
 include("includes/footer.php");
+?>
